288b30576f67f545978ede6d20a122ddf0559d6a
RendyStepanusS <rendivanbeck@gmail.com>
2020-12-17 11:00:31 +0700

    phpmvc

---
+++ App/core/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+class Controller
+{
+  public function view($view, $data = [])
+  {
+    require_once '../app/views/' . $view . '.php';
+  }
+
+  public function model($model)
+  {
+    require_once '../App/models/' . $model . '.php';
+    return new $model;
+  }
+}
